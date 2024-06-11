cb7dc056cebefb8c5dba8203d0aaa50a50480b00
azonecloud <100863730+azonecloud@users.noreply.github.com>
2024-06-11 13:23:16 +0530

    Dropped 9090 for MyApp

--- securitygroups.tf
+++ securitygroups.tf
@@ -41,20 +41,12 @@ resource "aws_vpc_security_group_ingress_rule" "allow_rdp" {
   to_port           = 3389
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_rdp" {
+resource "aws_vpc_security_group_ingress_rule" "allow_8080" {
   security_group_id = aws_security_group.main_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 8080
   ip_protocol       = "tcp"
   to_port           = 8080
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_rdp" {
-  security_group_id = aws_security_group.main_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 9090
-  ip_protocol       = "tcp"
-  to_port           = 9090
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
