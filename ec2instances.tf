550122a2ff620f177538ade1a39e750cc8f80936
azonecloud <100863730+azonecloud@users.noreply.github.com>
2024-06-13 10:57:48 +0530

    Workspace code Added

--- ec2instances.tf
+++ ec2instances.tf
@@ -1,5 +1,5 @@
 locals {
-  instance_type="a1.large"
+  instance_type = "a1.large"
 }
 
 resource "aws_instance" "main_instances1" {
@@ -15,3 +15,27 @@ resource "aws_instance" "main_instances2" {
   instance_type = local.instance_type
   key_name      = var.key_name
 }
+
+
+resource "aws_instance" "workspace_instances" {
+  ami           = var.ami_map[var.env]
+  instance_type = var.env == "prod" ? "m5.large" : "t3.micro"
+  key_name      = var.key_name
+
+  tags = {
+    Name = "{var.environment}-workspace-instance"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
+
--- launchtemplate.tf
+++ launchtemplate.tf
@@ -1,12 +1,12 @@
-locals {
-  image_id = lookup(var.region_map, var.region)
-}
+#locals {
+ # image_id = lookup(var.region_map, var.region)
+#}
 
 
 resource "aws_launch_template" "main_launch_template" {
   name_prefix = "main_launch_template"
-  #image_id              = var.ami_map[var.env]
-  image_id               = local.image_id
+  image_id              = var.ami_map[var.env]
+  #image_id               = local.image_id
   instance_type          = var.env == "prod" ? "m5.large" : "t3.micro"
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.main_sg.id]
